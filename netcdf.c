0334b72960f519ac9815812235f7a343e721cf8e
Adam Massmann <massmannak@gmail.com>
2018-03-09 16:56:19 -0500

    about to remove useless debugging

--- netcdf.c
+++ netcdf.c
@@ -1,1 +1,13 @@
 #include <netcdf.h>
+#include <stdio.h>
+
+extern int
+test(const char * path, int * ncidp)
+{
+  int ret0;
+  printf("path: %s", path);
+  ret0 = nc_open(path, NC_NOWRITE, ncidp);
+  return ret0;
+}
+
+
--- netcdf.scm
+++ netcdf.scm
@@ -12,6 +12,7 @@
                      (utf8->string new))))
 (display string)
 
+;;;; This uses ncdump to take a quick look at file contents
 (define (make-metadata filename)
   (let*
       ((string
@@ -24,6 +25,8 @@
        (joiner
         (string-joiner 'infix "\n")))
     (define (split-list newline-list startstr endstr)
+      ;; takes a list of ncdump's output split on new line
+      ;; returns a list spanning startstr to endstr
       (define (make-section section-list)
         (if (string=? (car section-list) endstr)
             '()
@@ -48,7 +51,8 @@
                              (filter var-filter?
                                      (split-list string-list "variables:"
                                                  "// global attributes:"))))
-          (apply joiner (split-list string-list "// global attributes:" "}")))))
+          (apply joiner (split-list string-list "// global attributes:" "}"))
+          filename)))
 
 (define (get-dimensions metadata)
   (let ((dims (car metadata)))
@@ -62,11 +66,55 @@
   (let ((attrs (caddr metadata)))
     (display attrs)
     attrs))
+(define (get-filepath metadata)
+  (cadddr metadata))
+
 
 ;; testing
 (define meta (make-metadata
               (string-append "/home/adam/scratch/data/"
                              "isccp/b1/GRIDSAT-B1.1987.05.03.18.v02r01.nc")))
+(display (get-filepath meta))
 (define dims (get-dimensions meta))
 (define vars (get-variables meta))
 (define attrs (get-attributes meta))
+
+
+;;; now we try loading with c lib
+(load-option 'ffi)
+(C-include "netcdf")
+
+(define string (let* ((alien (make-alien
+                              '(* char))))
+                 (C-call "nc_inq_libvers" alien)
+                 (let ((new (c-peek-cstring alien)))
+                   (if (alien-null? alien)
+                       (error "Could not open-file."))
+                   (if (string? new)
+                       new
+                       (utf8->string new)))))
+(display string)
+
+(define ncid (let* ((alien-out (make-alien))
+                    (bytevector (string->utf8
+                                 (string-append
+                                  "/home/adam/scratch/data/"
+                                  "isccp/b1/GRIDSAT-B1.1987.05.03.18.v02r01.nc")))
+                    (chars (malloc (1+ (* (c-sizeof "char")
+                                          (bytevector-length bytevector)))
+                                   '(* char)))
+                    (alien-mode (make-alien 'int))
+                    (alien-ncid (make-alien)))
+               (c-call "test"
+                       alien-out
+                       (string->utf8 (string-append
+                                      "/home/adam/scratch/data/"
+                                      "isccp/b1/GRIDSAT-B1.1987.05.03.18.v02r01.nc"))
+                       alien-ncid)
+              (if (alien-null? alien-out)
+                  (display "eroor could't open"))
+              alien-ncid))
+
+
+
+
