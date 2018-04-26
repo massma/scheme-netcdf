899a84a5e8a8d816b7f0ee17966d0c317d403401
Adam Massmann <massmannak@gmail.com>
2018-04-26 18:51:50 -0400

    okay on stable MIT branch, just need to fix string calls in meta data (maybe use FFI?)

--- optiondb.scm
+++
@@ -1,8 +0,0 @@
-#| -*-Scheme-*- |#
-
-;;;; Test optiondb, includes the installed system's optiondb.
-
-(further-load-options
- (merge-pathnames "optiondb"
-		  (cadr (access library-directory-path
-				(->environment '(runtime pathname))))))
